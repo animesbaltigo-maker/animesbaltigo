6faa3ae3624366810ee3d5463c02dc3380245805
animesbaltigo-maker <animesbaltigo@gmail.com>
2026-03-26 17:24:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8675150552:AAHoUu64RoMPHNdaChP9RQGF0iz-tk7Crbo").strip()
 SOURCE_SITE_BASE = os.getenv("SOURCE_SITE_BASE", "https://animefire.io").strip().rstrip("/")
 
 REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "@Centraldeanimes_Baltigo").strip()
 REQUIRED_CHANNEL_URL = os.getenv("REQUIRED_CHANNEL_URL", "t.me/Centraldeanimes_Baltigo").strip()
+BOT_USERNAME = "AnimesBaltigo_Bot"
+CANAL_POSTAGEM = "@Centraldeanimes_Baltigo"
+STICKER_DIVISOR = "CAACAgQAAx0CbKkU-AACFJtps_kRLpeUt2Gvd7mT4d0gS1vyCgACOhUAAqDAiFJSU5pkUMltvzoE"
+ADMIN_IDS = [1852596083, 987654321]
+
 
 SEARCH_LIMIT = int(os.getenv("SEARCH_LIMIT", "10"))
 EPISODES_PER_PAGE = int(os.getenv("EPISODES_PER_PAGE", "12"))
