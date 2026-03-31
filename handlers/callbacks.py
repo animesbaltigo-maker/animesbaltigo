36edddf6997c2b6a2751f0583a1bd7d89a7fed13
animesbaltigo-maker <animesbaltigo@gmail.com>
2026-03-31 15:41:17 -0400

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -37,7 +37,7 @@ RECOMMEND_CACHE_TTL = 60 * 5
 GLOBAL_FETCH_SEMAPHORE = asyncio.Semaphore(40)
 
 SEARCH_BANNER_URL = "https://photo.chelpbot.me/AgACAgEAAxkBaL-UMWnDPUdoNCaz4ZUFvzeOHSVXh0oRAALTC2sbdnEYRrjsVpeCeT08AQADAgADeQADOgQ/photo.jpg"
-MINIAPP_URL = "https://car-educational-assess-interactions.trycloudflare.com/app"
+MINIAPP_URL = "https://modem-satisfied-inquire-pixels.trycloudflare.com/app"
 
 GENRE_PT_MAP = {
     "Action": "Ação",
