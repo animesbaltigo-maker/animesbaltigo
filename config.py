262c4ee5aa08e94ff4af5bad8e724a957fc00441
animesbaltigo-maker <animesbaltigo@gmail.com>
2026-04-29 20:08:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -60,5 +60,5 @@ VIDEO_DOWNLOAD_PROTECT_CONTENT = os.getenv("VIDEO_DOWNLOAD_PROTECT_CONTENT", "1"
 BOT_BRAND = os.getenv("BOT_BRAND", "Anime Brasil").strip()
 WEBAPP_BASE_URL = os.getenv("WEBAPP_BASE_URL", "").strip().rstrip("/")
 BALTIGOFLIX_SUBSCRIBE_URL = os.getenv("BALTIGOFLIX_SUBSCRIBE_URL", "http://baltigoflix.com.br/").strip()
-BALTIGOFLIX_SUPPORT_URL = os.getenv("BALTIGOFLIX_SUPPORT_URL", "https://t.me/SourceBaltigo_Bot").strip()
-CAKTO_WEBHOOK_SECRET = os.getenv("CAKTO_WEBHOOK_SECRET", "").strip()
+BALTIGOFLIX_SUPPORT_URL = os.getenv("BALTIGOFLIX_SUPPORT_URL", "https://t.me/QGSuporteBot").strip()
+CAKTO_WEBHOOK_SECRET = os.getenv("CAKTO_WEBHOOK_SECRET", "0ce52ce5-aa98-4aff-be81-23ccbb85f741").strip()
